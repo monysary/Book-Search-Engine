7b43d2b81e43fc84e399c4970936d75db5c99765
monysary <sary.mony@gmail.com>
2023-01-27 13:57:16 -0800

    set up mutations in client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,17 +1,67 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN = gql `
-
+    mutation login ($email: String!, $password, String!) {
+        login (email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+                bookCount
+            }
+        }
+    }
 `
 
 export const ADD_USER = gql`
-
+    mutation addUser ($username: String!, $email: String!, $password: String!) {
+        addUser (username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+                bookCount
+            }
+        }
+    }
 `
 
 export const SAVED_BOOK = gql`
-
+    mutation savedBook ($bookData: BookInput!) {
+        savedBook (bookData: $bookData) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
 `
 
 export const REMOVE_BOOK = gql`
-
+    mutation removeBook ($bookId: ID!) {
+        removeBook (bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
 `
