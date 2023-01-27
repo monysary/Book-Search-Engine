f2aebcc187288a5b7869620c33c966441778d808
monysary <sary.mony@gmail.com>
2023-01-27 13:49:15 -0800

    set up templatee for mutations on client side

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,17 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql `
+
+`
+
+export const ADD_USER = gql`
+
+`
+
+export const SAVED_BOOK = gql`
+
+`
+
+export const REMOVE_BOOK = gql`
+
+`
